28bb2d1c5ec168b9ec7e3d61dfbbb1a59bb75adf
tbegerow <mobil@be-com.de>
2026-02-15 09:49:54 +0100

    Release 4.22-ng

--- ESPHamClock/version.cpp
+++ ESPHamClock/version.cpp
@@ -1,3 +1,9 @@
 // N.B. if beta be sure to use 2 places: 4.09b01
+// original Code by Elwwod Charles Downey WB0EW (sk)
+//const char *hc_version = "4.21";
 
-const char *hc_version = "4.21";
+#ifndef HAMCLOCK_VERSION
+#define HAMCLOCK_VERSION "4.22-ng"
+#endif
+
+const char *hc_version = HAMCLOCK_VERSION;
